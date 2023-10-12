6d5f92d2ee662954636bf0deffd4ca4b32680e1b
yeetzpxoci <yigityigin@hotmail.com>
2023-10-12 11:54:16 +0200

    editing genre implemented

--- server/controllers/genreController.js
+++ server/controllers/genreController.js
@@ -1,10 +1,28 @@
 const Genre = require("../models/genre");
 const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require('express-validator');
 
 exports.genreList = asyncHandler(async (req, res, next) => {
     const genreList = await Genre.find({}).exec();
     res.send(genreList);
 });
+
+exports.validateGenreInput = [
+    (req, res, next) => {
+        next();
+    },
+
+    body('name').notEmpty().withMessage('Name must not be empty').escape(),
+    body('imgUrl').optional().escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+]
 
 exports.addGenre = asyncHandler(async (req, res, next) => {
     const newGenre = new Genre({
@@ -27,6 +45,22 @@ exports.deleteGenre = asyncHandler(async (req, res, next) => {
         res.status(201).json({ message: 'Genre deleted successfully' });
     } catch (error) {
         console.error('Error during save:', error);
+        res.status(500).json({ error: error.message });
+    }
+})
+
+exports.updateGenre = asyncHandler(async (req, res, next) => {
+    const newGenre = new Genre({
+        _id: req.body._id,
+        name: req.body.name,
+        imgURL: req.body.imgURL
+    });
+
+    try {
+        await Genre.findByIdAndUpdate(newGenre._id, newGenre);
+        res.status(201).json({ message: 'Genre updated successfully' });
+    } catch (error) {
+        console.error('Error during update:', error);
         res.status(500).json({ error: error.message });
     }
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,17 +9,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/games', gameController.gameList);
-
 router.post('/games/add', gameController.validateGameInput, gameController.addGame);
-
 router.post('/games/delete', gameController.deleteGame);
-
 router.post('/games/update', gameController.validateGameInput, gameController.updateGame);
 
 router.get('/genres', genreController.genreList);
-
-router.post('/genres/add', genreController.addGenre);
-
+router.post('/genres/add', genreController.validateGenreInput, genreController.addGenre);
 router.post('/genres/delete', genreController.deleteGenre);
+router.post('/genres/update', genreController.validateGenreInput, genreController.updateGenre);
 
 module.exports = router;
